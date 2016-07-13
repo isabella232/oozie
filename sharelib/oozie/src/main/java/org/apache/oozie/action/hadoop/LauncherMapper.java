77318d3a9773d1cda8fb36dfce790c563e71aa00
Rohini Palaniswamy <rohini@apache.org>
2016-07-13 14:11:32 -0700

    OOZIE-2581 Oozie should reset SecurityManager in finally block (satishsaley via rohini)
    (cherry picked from commit aa6e2eb44c20b153de27afa60f91e8f765bc09a2)
    
    Change-Id: I3e8a8ba454b2c11d86e401c4b8de9a3a7a193f66
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/15042
    Tested-by: Jenkins User
    Reviewed-by: Robert Kanter <rkanter@cloudera.com>
    (cherry picked from commit 05ddd0d7cd40b9da995417a7590e04c865ab19db)
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/15475

--- sharelib/oozie/src/main/java/org/apache/oozie/action/hadoop/LauncherMapper.java
+++ sharelib/oozie/src/main/java/org/apache/oozie/action/hadoop/LauncherMapper.java
@@ -28,8 +28,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.io.OutputStream;
-import java.io.FileOutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.security.Permission;
@@ -149,6 +147,7 @@ public class LauncherMapper<K1, V1, K2, V2> implements Mapper<K1, V1, K2, V2>, R
 
     @Override
     public void map(K1 key, V1 value, OutputCollector<K2, V2> collector, Reporter reporter) throws IOException {
+        SecurityManager initialSecurityManager = System.getSecurityManager();
         try {
             if (configFailure) {
                 throw configureFailureEx;
@@ -162,7 +161,6 @@ public class LauncherMapper<K1, V1, K2, V2> implements Mapper<K1, V1, K2, V2>, R
                 int errorCode = 0;
                 Throwable errorCause = null;
                 String errorMessage = null;
-
                 try {
                     new LauncherSecurityManager();
                 }
@@ -320,6 +318,7 @@ public class LauncherMapper<K1, V1, K2, V2> implements Mapper<K1, V1, K2, V2>, R
         }
         finally {
             uploadActionDataToHDFS();
+            resetSecurityManager(initialSecurityManager);
         }
     }
 
@@ -597,6 +596,19 @@ public class LauncherMapper<K1, V1, K2, V2> implements Mapper<K1, V1, K2, V2>, R
                     }
                 }
             }
+        }
+    }
+
+    private void resetSecurityManager(SecurityManager initialSecurityManager) {
+        try {
+            SecurityManager prev = System.getSecurityManager();
+            System.setSecurityManager(initialSecurityManager);
+            System.out
+                    .println("Successfully reset security manager from " + prev + " to " + System.getSecurityManager());
+        }
+        catch (Throwable t) {
+            System.err.println("Failed to reset security manager: " + t.getMessage());
+            t.printStackTrace(System.err);
         }
     }
 
