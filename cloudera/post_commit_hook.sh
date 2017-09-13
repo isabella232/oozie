f65063829156d17fcb51d9c2f636682518d10d29
Gezapeti Cseh <gezapeti@apache.org>
2017-09-13 11:03:25 +0200

    CLOUDERA-BUILD: CDH-59114 Fix commit flow alerts
    Change-Id: Iab1122b0bcbd3d397ff8f8e33d8c227bc1d2b734
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/26507
    Reviewed-by: Peter Cseh <gezapeti@cloudera.com>
    Tested-by: Peter Cseh <gezapeti@cloudera.com>

--- cloudera/post_commit_hook.sh
+++ cloudera/post_commit_hook.sh
@@ -1,1 +1,8 @@
-test-distributed.sh
+#!/bin/bash
+set -xe
+
+DIR="$( cd $( dirname ${BASH_SOURCE[0]} )  && pwd )"
+# we run mr1 tests only after promotion
+./bin/mkdistro.sh -Pmr1 -DskipTests
+# run tests with mr2
+./bin/mkdistro.sh
