5a50a989db8e0cde588a16bda161ccf7979cd36b
satishsaley <satishsaley@apache.org>
2018-01-06 12:26:14 -0800

    Fix filesystem issue (satishsaley, asasvari, gezapeti)
    (cherry picked from commit b98c37eeaf993e37f87225a114fb6cedce174168)
    
    Change-Id: I5c5cbe61604e22f112204c130ebbdcab099039c5
    (cherry picked from commit f1a8e174db5744ffdafcf97431c5de4952816edb)

--- core/src/main/java/org/apache/oozie/action/hadoop/JavaActionExecutor.java
+++ core/src/main/java/org/apache/oozie/action/hadoop/JavaActionExecutor.java
@@ -1014,6 +1014,11 @@ public class JavaActionExecutor extends ActionExecutor {
             String consoleUrl;
             String launcherId = LauncherHelper.getRecoveryId(launcherConf, context.getActionDir(), context
                     .getRecoveryId());
+
+            removeHBaseSettingFromOozieDefaultResource(launcherConf);
+            removeHBaseSettingFromOozieDefaultResource(actionConf);
+
+
             boolean alreadyRunning = launcherId != null;
 
             // if user-retry is on, always submit new launcher
@@ -1057,6 +1062,16 @@ public class JavaActionExecutor extends ActionExecutor {
             if (yarnClient != null) {
                 Closeables.closeQuietly(yarnClient);
             }
+        }
+    }
+
+    private void removeHBaseSettingFromOozieDefaultResource(final Configuration jobConf) {
+        final String[] propertySources = jobConf.getPropertySources(HbaseCredentials.HBASE_USE_DYNAMIC_JARS);
+        if (propertySources != null && propertySources.length > 0 &&
+                propertySources[0].contains(HbaseCredentials.OOZIE_HBASE_CLIENT_SITE_XML)) {
+            jobConf.unset(HbaseCredentials.HBASE_USE_DYNAMIC_JARS);
+            LOG.debug(String.format("Unset [%s] inserted from default Oozie resource XML [%s]",
+                    HbaseCredentials.HBASE_USE_DYNAMIC_JARS, HbaseCredentials.OOZIE_HBASE_CLIENT_SITE_XML));
         }
     }
 
