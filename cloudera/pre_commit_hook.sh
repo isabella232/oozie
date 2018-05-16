9d465884c2dfdf2f6734d969030d1fc7af4bf0bd
Gezapeti Cseh <gezapeti@apache.org>
2018-05-16 16:55:04 +0200

    CLOUDERA-BUILD: RELENG-3353 Don't use grind in pre-commit hook
    Change-Id: I8426c6d8cf2bee05fcbbbddbd562bd02ad66103e

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -1,1 +1,13 @@
-test-distributed.sh
+# This is a temporary fix until we get the distributed test framework
+# up and running in pre-commit
+
+#!/bin/bash
+set -xe
+# From jenkins
+export JAVA8_HOME=$JAVA_1_8_HOME
+
+
+# activate mvn-gbn wrapper
+mv "$(which mvn-gbn-wrapper)" "$(dirname "$(which mvn-gbn-wrapper)")/mvn"
+
+mvn clean install -B -fae
