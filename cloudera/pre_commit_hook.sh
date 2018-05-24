9661082e432dc19fd4307023808892d04c12c651
Gezapeti Cseh <gezapeti@apache.org>
2018-05-24 17:19:24 +0200

    CLOUDERA-BUILD: Fix pre-commit
    Change-Id: I1285f53b2e00818a88d399c0a2e7deac87e96993

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -1,13 +1,13 @@
 # This is a temporary fix until we get the distributed test framework
 # up and running in pre-commit
 
-#!/bin/bash
-set -xe
-# From jenkins
-export JAVA8_HOME=$JAVA_1_8_HOME
-
-
-# activate mvn-gbn wrapper
-mv "$(which mvn-gbn-wrapper)" "$(dirname "$(which mvn-gbn-wrapper)")/mvn"
-
-mvn clean install -B -fae
+#!/bin/bash -xe
+export JAVA8_BUILD=true
+. /opt/toolchain/toolchain.sh
+export branchName="cdh6.x"
+export PATH=$MAVEN_3_5_0_HOME/bin:$PATH
+export CDH_GBN=$(curl "http://builddb.infra.cloudera.com:8080/resolvealias?alias=${branchName}")
+env
+curl http://github.mtv.cloudera.com/raw/CDH/cdh/${branchName}/gbn-m2-settings.xml > mvn_settings.xml
+JAVA8_BUILD=true
+mvn clean test -s mvn_settings.xml -B -Dmaven.test.failure.ignore=true -fae -Doozie.test.waitfor.ratio=2 -Dtest.timeout=10800 -Dmaven.test.java.opts="-Xmx2048m -da"
