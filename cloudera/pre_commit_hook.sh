3e0222a4838cf32cc8671758ebb7a5504b7810ea
Andras Salamon <asalamon@cloudera.com>
2019-02-12 11:45:47 +0100

    CLOUDERA-BUILD: CDH-77690 - Eliminate hardwired branch name from Oozie pre_commit_hook.sh
    Change-Id: I84b43062f07259b3ed8ace32a290e0df5f1f2502

--- cloudera/pre_commit_hook.sh
+++ cloudera/pre_commit_hook.sh
@@ -1,13 +1,12 @@
+#!/bin/bash -xe
 # This is a temporary fix until we get the distributed test framework
 # up and running in pre-commit
-
-#!/bin/bash -xe
 export JAVA8_BUILD=true
 . /opt/toolchain/toolchain.sh
-export branchName="cdh6.x"
 export PATH=$MAVEN_3_5_0_HOME/bin:$PATH
-export CDH_GBN=$(curl "http://builddb.infra.cloudera.com:8080/resolvealias?alias=${branchName}")
+CDH_GBN=$(curl "http://builddb.infra.cloudera.com:8080/resolvealias?alias=${GIT_LOCAL_BRANCH}")
+export CDH_GBN
 env
-curl https://github.infra.cloudera.com/raw/CDH/cdh/${branchName}/gbn-m2-settings.xml > mvn_settings.xml
+curl "https://github.infra.cloudera.com/raw/CDH/cdh/${GIT_LOCAL_BRANCH}/gbn-m2-settings.xml" > mvn_settings.xml
 JAVA8_BUILD=true
 mvn clean test -s mvn_settings.xml -B -fae -Doozie.test.waitfor.ratio=2 -Dtest.timeout=10800 -Dmaven.test.java.opts="-Xmx2048m -da"
